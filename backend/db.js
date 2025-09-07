f88ca61b496589bf20ae9425290a57e1e11eb9fa
D3sc <vinary686@gmail.com>
2025-09-07 19:36:54 +0700

    setup: setup with prisma mysql

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,4 @@
-const mysql = require('mysql2');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const connection = mysql.createConnection({
-  host: 'mysql',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("MySQL not ready, retrying in 5s...", err.message);
-    setTimeout(connectWithRetry, 5000);
-  } else {
-    console.log("Database connected!");
-  }
-});
-
-module.exports = connection;
+module.exports = prisma;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connection = require('./db');
+const prisma = require('./db');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -10,6 +10,15 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
+
+app.get("/users", async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
