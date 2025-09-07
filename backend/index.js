b7e67c2486eed4dffb3e09a956978287398c9a06
D3sc <vinary686@gmail.com>
2025-09-07 15:47:34 +0700

    fix bug: setup the ip 0.0.0.0

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connection = require('./db');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
